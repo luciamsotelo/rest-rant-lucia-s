0b99c3f7a0b0f8f0d3293179591aaa361ce6d4b0
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2023-11-15 18:56:42 -0800

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,19 +1,28 @@
+const express = require("express");
 const router = require("express").Router();
 
 // GET /places
+router.get('/new', (req, res) => {
+    res.render('places/new')
+});
+
 router.get("/", (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: '/images/h-thai-ml-tables.png'
+        pic: '/images/h-thai-ml-tables.jpg', 
+        // Photo by <a href="https://unsplash.com/@clemono?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Clem Onojeghuo</a> on <a href="https://unsplash.com/photos/person-sitting-inside-restaurant-zlABb6Gke24?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
 }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
         pic: '/images/coffee-cat.jpg'
+        // Photo by <a href="https://unsplash.com/@robertbye?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Robert Bye</a> on <a href="https://unsplash.com/photos/muffin-served-on-brown-paper-5LBIuI7c_ps?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
 }]
 
     res.render('places/index', { places });
--- views/error404.jsx
+++ views/error404.jsx
@@ -6,13 +6,14 @@ return (
     <Def>
         <main>
             <h1>404: PAGE NOT FOUND</h1>
+            <p>Oops, sorry, we can't find this page!</p>
             <div>
                 <img src="/images/puppy-cute-404.jpg" alt="cute puppy" style={{width: "50%"}}/>
                 <div>
                     Photo by <a href="AUTHOR_LINK">Michael G</a> on <a href="UNSPLASH_LINK">Unsplash</a>
                 </div>
             </div>
-            <p>Oops, sorry, we can't find this page!</p>
+            
         </main>
     </Def>
     )
