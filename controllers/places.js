5904379eb4269079132283c20dd4155d952142b8
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2023-11-15 20:01:04 -0800

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = require("express").Router();
+const places = require("../models/places.js");
+
+router.get('/', (req, res) => {
+    res.render('places/index', {places})
+});
 
 // GET /places
 router.get('/new', (req, res) => {
@@ -7,7 +12,19 @@ router.get('/new', (req, res) => {
 });
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
   
 
---
+++ models/places.js
@@ -0,0 +1,17 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/h-thai-ml-tables.jpg', 
+    // Photo by <a href="https://unsplash.com/@clemono?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Clem Onojeghuo</a> on <a href="https://unsplash.com/photos/person-sitting-inside-restaurant-zlABb6Gke24?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-cat.jpg'
+    // Photo by <a href="https://unsplash.com/@robertbye?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Robert Bye</a> on <a href="https://unsplash.com/photos/muffin-served-on-brown-paper-5LBIuI7c_ps?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+}]
