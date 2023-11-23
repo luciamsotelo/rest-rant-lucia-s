05e62d9b78c38ddf641a9d8f41e319fea8092713
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2023-11-22 21:30:54 -0800

    assignment

--- controllers/places.js
+++ controllers/places.js
@@ -27,9 +27,16 @@ router.get("/new", (req, res) => {
     res.render("places/new");
 });
 
-router.get("/:id", (req, res) => {
-    res.send("GET /places/:id stub");
-});
+router.get('/:id', (req, res) => {
+    db.Place.findById(req.params.id)
+    .then(place => {
+        res.render('places/show', { place })
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
+})
 
 router.put("/:id", (req, res) => {
     res.send("PUT /places/:id stub");
