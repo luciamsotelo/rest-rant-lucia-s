9c9b14cc15daa6e5eacbe0c865e6bf30514e2a1d
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2023-12-02 14:34:29 -0800

    fix errors

--- controllers/places.js
+++ controllers/places.js
@@ -115,8 +115,8 @@ router.post("/:id/comment", (req, res) => {
 
 
 
-router.delete("/:id/rant/:rantId", (req, res) => {
-    res.send("GET /places/:id/rant/:rantId stub");
-});
+// router.delete("/:id/rant/:rantId", (req, res) => {
+//     res.send("GET /places/:id/rant/:rantId stub");
+// });
 
 module.exports = router;
